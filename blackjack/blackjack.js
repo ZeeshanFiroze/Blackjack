2743afd98f6553dee10988759dafad4f5c18e32a
ZeeshanFiroze <zeeshanfirozeabdulla@gmail.com>
2026-01-07 22:19:51 +0530

    Created a simple blackjack game

---
+++ blackjack/blackjack.js
@@ -0,0 +1,34 @@
+let firstCard = 10;
+let secondCard = 8;
+let sum = firstCard + secondCard;   
+let hasBlackJack = false;
+let isAlive = true;
+let message = ""; 
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+function startGame(){
+if (sum <= 20)
+{
+    message = "Do you want to draw a new card?"
+}
+else if (sum === 21)
+{
+    message = "Blackjack! You win!";
+    hasBlackJack = true;
+} 
+else
+{
+    message = "You are out of the game!";
+    isAlive = false; 
+}
+messageEl.textContent = message;
+sumEl.textContent = "Sum: " + sum;
+cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+}
+function newCard(){
+    let newCard = 6;
+    sum += newCard;
+    startGame();
+    cardsEl.textContent += " " + newCard; 
+}
