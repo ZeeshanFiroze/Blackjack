7426ec30df9573ac5a34f060461feef4a4b13aeb
ZeeshanFiroze <zeeshanfirozeabdulla@gmail.com>
2026-01-13 21:25:26 +0530

    Improved the newCard button

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -1,19 +1,33 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;   
+let cards = [];
+let sum = 0;   
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = ""; 
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 function startGame(){
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    isAlive = true;
      renderGame()
 }
+let player = {
+    name: "Zeeshan",
+    chips: 145
+}
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 function getRandomCard()
 {
     let randomnumber = Math.floor(Math.random() * 13 ) + 1;
+    if(randomnumber == 1)
+    return 11;
+    else if(randomnumber > 10)
+    return 10; 
+    else
     return randomnumber;
 }
 function renderGame(){
@@ -32,6 +46,7 @@ else if (sum === 21)
 {
     message = "Blackjack! You win!";
     hasBlackJack = true;
+    isAlive = false;
 } 
 else
 {
@@ -43,9 +58,12 @@ messageEl.textContent = message;
 }
 function newCard()
 {
-    let Card = getRandomCard();
-    sum += Card;
-    cards.push(Card);
-    renderGame();
+    if(isAlive === true && hasBlackJack === false)
+    {
+     let Card = getRandomCard();
+     sum += Card;
+     cards.push(Card);
+     renderGame();
+    }
 }
 
